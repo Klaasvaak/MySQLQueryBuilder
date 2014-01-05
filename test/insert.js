77a1160a6f67d6adc665ddbbe3be30113d145482
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-01-05 22:00:33 +0100

    set now accepts an object.

--- test/insert.js
+++ test/insert.js
@@ -7,6 +7,7 @@ var insert = require('../index.js').insert;
  * INSERT INTO `students` (`name`) VALUES ('Jon')
  * INSERT INTO `students` (`name`, `score`) VALUES ('Jon', 10)
  * INSERT INTO `students` (`name`, `score`, `school`) VALUES ('Jon', 10, NULL)
+ * INSERT INTO `students` (`name`, `score`, `school`) VALUES ('Jon', 10, NULL) # With object
  */
 
 describe('Insert', function() {
@@ -33,6 +34,15 @@ describe('Insert', function() {
             .set('name', 'Jon')
             .set('score', 10)
             .set('school', null)
+            .toString();
+        expect(q).to.be.equal("INSERT INTO `students` (`name`, `score`, `school`) VALUES ('Jon', 10, NULL)");
+    });
+
+    it("INSERT INTO `students` (`name`, `score`, `school`) VALUES ('Jon', 10, NULL) # With object", function() {
+        var jon = {name: 'Jon', score: 10, school: null}
+        var q = insert()
+            .into('students')
+            .set(jon)
             .toString();
         expect(q).to.be.equal("INSERT INTO `students` (`name`, `score`, `school`) VALUES ('Jon', 10, NULL)");
     });
